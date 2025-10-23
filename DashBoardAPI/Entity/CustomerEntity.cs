6448e26b4dc9dd0c5b7993e61c2c69b8eb3eb1ab
Sonu Rajbhar <sonu296702@gmail.com>
2025-10-23 16:35:27 +0530

    ,,

--- DashBoardAPI/Entity/CustomerEntity.cs
+++ DashBoardAPI/Entity/CustomerEntity.cs
@@ -1,11 +1,18 @@
-﻿namespace DashBoardAPI.Entity
+﻿using System.Text.Json.Serialization;
+
+namespace DashBoardAPI.Entity
 {
     public class CustomerEntity:BaseEntity
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; } = "";
+        [JsonPropertyName("mobile")]
         public string Mobile { get; set; } = "";
+        [JsonPropertyName("email")]
         public string Email { get; set; } = "";
+        [JsonPropertyName("address")]
         public string Address { get; set; } = "";
     }
 }
